28b10b99414ffb224737a2b4868c042b9471cb2c
Luiz Fernando <luizfernando987@gmail.com>
2026-03-03 21:38:21 -0300

    Aula 03 - Classes e Enuns

---
+++ Models/Funcionario.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Aula03RH.Models.Enuns;
+
+
+
+namespace Aula03RH.Models
+{
+    public class Funcionario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = "";
+        public string Cpf { get; set; } = string.Empty;
+        public DateTime DataAdmissao { get; set; }
+        public decimal Salario { get; set; }
+        public TipoFuncionarioEnum TipoFuncionario { get; set; }
+
+        public void ReajustarSalario()
+        {
+            Salario = Salario + (Salario * 10 / 100);
+        }
+
+        public decimal CalcularDescontoVT(decimal percentual)
+        {
+            decimal desconto = Salario * percentual/100;
+            return desconto;
+        }
+
+
+
+
+    }
+}
